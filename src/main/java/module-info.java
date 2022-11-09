864a34903629c8a49cf545e392ef120ff9e09f3c
JuniorSelister <junior.selister@gmail.com>
2022-11-09 14:01:28 -0300

    Uploading project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.softwareinstaller.sir {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.softwareinstaller.sir to javafx.fxml;
+    exports com.softwareinstaller.sir;
+}
